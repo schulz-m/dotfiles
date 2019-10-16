81681799893e87c83cde31baf639f36749190936
Maximilian Schulz <max@unitelabs.ch>
2019-10-16 21:51:45 +0200

    Add bash function to clean stale branches

--- common/.git.bash
+++ common/.git.bash
@@ -1,6 +1,21 @@
+#!/bin/bash
+
 # GIT Helpers
-git_notrack() {
-    git branch -vv | awk '/: gone]/{print $1}'
+function git_notrack {
+     git branch -vv | awk '{print $1, $4}' | grep 'gone]' | awk '{print $1}'
+}
+
+function git_clean {
+	git fetch origin
+	git remote prune origin
+	DEAD_BRANCHES="$(git_notrack)"
+	if test -z "$DEAD_BRANCHES"
+	then
+		echo "No branches found to clean"
+    else
+    	echo "Deleting dead branches: $DEAD_BRANCHES"
+    	git branch -D $DEAD_BRANCHES
+    fi
 }
 
 export GITAWAREPROMPT=${SCRIPT_DIR}/git-aware-prompt
