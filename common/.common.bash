b10378277f4e267264521794da0c60edc6c79216
Maximilian Schulz <schulm56@roche.com>
2023-06-14 10:55:11 +0200

    Add human readable into ll

--- common/.common.bash
+++ common/.common.bash
@@ -2,7 +2,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Bash aliases helpers
 alias l='ls'
-alias ll='ls -l'
+alias ll='ls -lh'
 
 # Directory jump with Z Shell, taken from https://github.com/rupa/z
 . ${SCRIPT_DIR}/z/z.sh
